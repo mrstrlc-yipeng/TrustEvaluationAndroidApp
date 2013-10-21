020a47ab00ca164a8e337f7e7be9041f156f9f81
yipeng <huang.ypeng@gmail.com>
2013-10-21 06:17:58 +0200

    list 1, local contacts
    display the list of local contacts from phone book

--- gen/fr/utt/isi/tx/trustevaluationandroidapp/R.java
+++ gen/fr/utt/isi/tx/trustevaluationandroidapp/R.java
@@ -24,18 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int contactEntryText=0x7f080001;
+        public static final int contactList=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_list_contact_splitted=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int contact_entry=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int list_contact_splitted=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int button_contacts=0x7f050004;
+        public static final int development_env=0x7f050003;
+        public static final int title_activity_list_contact_splitted=0x7f050002;
     }
     public static final class style {
         /** 
