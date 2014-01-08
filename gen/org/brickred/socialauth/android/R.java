fc63a28ccc2205911f91c5a857cdd53215fca1cd
yipeng <huang.ypeng@gmail.com>
2014-01-08 23:15:34 +0100

    add landscape layout, add network check

--- gen/org/brickred/socialauth/android/R.java
+++ gen/org/brickred/socialauth/android/R.java
@@ -8,20 +8,20 @@ package org.brickred.socialauth.android;
 
 public final class R {
 	public static final class drawable {
-		public static final int email = 0x7f02007c;
-		public static final int facebook = 0x7f02007d;
-		public static final int flickr = 0x7f02007e;
-		public static final int foursquare = 0x7f02007f;
-		public static final int google = 0x7f020080;
-		public static final int googleplus = 0x7f020081;
-		public static final int instagram = 0x7f020087;
-		public static final int linkedin = 0x7f020088;
-		public static final int mms = 0x7f020089;
-		public static final int myspace = 0x7f02008a;
-		public static final int runkeeper = 0x7f02008b;
-		public static final int salesforce = 0x7f02008c;
-		public static final int twitter = 0x7f02008d;
-		public static final int yahoo = 0x7f02008e;
-		public static final int yammer = 0x7f02008f;
+		public static final int email = 0x7f02007d;
+		public static final int facebook = 0x7f02007f;
+		public static final int flickr = 0x7f020081;
+		public static final int foursquare = 0x7f020082;
+		public static final int google = 0x7f020083;
+		public static final int googleplus = 0x7f020084;
+		public static final int instagram = 0x7f02008a;
+		public static final int linkedin = 0x7f02008b;
+		public static final int mms = 0x7f02008e;
+		public static final int myspace = 0x7f02008f;
+		public static final int runkeeper = 0x7f020090;
+		public static final int salesforce = 0x7f020091;
+		public static final int twitter = 0x7f020092;
+		public static final int yahoo = 0x7f020094;
+		public static final int yammer = 0x7f020095;
 	}
 }
--- src/fr/utt/isi/tx/trustevaluationandroidapp/adapters/ContactNodeListAdapter.java
+++ src/fr/utt/isi/tx/trustevaluationandroidapp/adapters/ContactNodeListAdapter.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import fr.utt.isi.tx.trustevaluationandroidapp.R;
 import fr.utt.isi.tx.trustevaluationandroidapp.database.TrustEvaluationDataContract;
@@ -15,9 +17,11 @@ import fr.utt.isi.tx.trustevaluationandroidapp.models.MergedContactNode;
 
 public class ContactNodeListAdapter extends ArrayAdapter<MergedContactNode> {
 
-	//private static final String TAG = "ContactNodeListAdapter";
+	private static final String TAG = "ContactNodeListAdapter";
 
 	private Context context;
+	
+	private int textViewResourceId;
 
 	private List<MergedContactNode> contactNodes;
 	
@@ -28,6 +32,7 @@ public class ContactNodeListAdapter extends ArrayAdapter<MergedContactNode> {
 		super(context, textViewResourceId, contactNodes);
 
 		this.context = context;
+		this.textViewResourceId = textViewResourceId;
 		this.contactNodes = contactNodes == null ? new ArrayList<MergedContactNode>() : contactNodes;
 	}
 
@@ -39,10 +44,10 @@ public class ContactNodeListAdapter extends ArrayAdapter<MergedContactNode> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
-		if (view == null) {
+		if (view == null || textViewResourceId == R.layout.contact_node_complete_list) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.contact_node_list, null);
+			view = inflater.inflate(textViewResourceId, null);
 		}
 
 		MergedContactNode listElement = contactNodes.get(position);
@@ -63,6 +68,36 @@ public class ContactNodeListAdapter extends ArrayAdapter<MergedContactNode> {
 				score = "N/A";
 			}
 			scoreView.setText("" + score);
+			
+			// set source check image into view if the layout is R.layout.contact_node_complet_list
+			if (textViewResourceId == R.layout.contact_node_complete_list) {
+				Log.v(TAG, listElement.getDisplayNameGlobal() + ": " + listElement.getIsLocalPhone() + " " + listElement.getIsLocalEmail() + " " + listElement.getIsFacebook() + " " + listElement.getIsTwitter() + " " + listElement.getIsLinkedin());
+
+				if (listElement.getIsLocalPhone() == 1) {
+					ImageView isLocalPhoneImageView = (ImageView) view.findViewById(R.id.is_local_phone);
+					isLocalPhoneImageView.setImageResource(R.drawable.local_phone_logo);
+				}
+				
+				if (listElement.getIsLocalEmail() == 1) {
+					ImageView isLocalEmailImageView = (ImageView) view.findViewById(R.id.is_local_email);
+					isLocalEmailImageView.setImageResource(R.drawable.email_logo);
+				}
+				
+				if (listElement.getIsFacebook() == 1) {
+					ImageView isFacebookImageView = (ImageView) view.findViewById(R.id.is_facebook);
+					isFacebookImageView.setImageResource(R.drawable.facebook_logo);
+				}
+				
+				if (listElement.getIsTwitter() == 1) {
+					ImageView isTwitterImageView = (ImageView) view.findViewById(R.id.is_twitter);
+					isTwitterImageView.setImageResource(R.drawable.twitter_logo);
+				}
+				
+				if (listElement.getIsLinkedin() == 1) {
+					ImageView isLinkedinImageView = (ImageView) view.findViewById(R.id.is_linkedin);
+					isLinkedinImageView.setImageResource(R.drawable.linkedin_logo);
+				}
+			}
 		}
 
 		return view;
